59c76fade98f3af62b3cdf6d6e327e53d8fd6a9a
gilTads <gil.tads@gmail.com>
2020-05-13 21:53:14 -0400

    initial troo tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,9 +1,23 @@
-describe('Authentication', () => {
-  it('should sum two numbers', () => {
-    const x = 2
-    const y = 4
-    const sum = x + y
+const request = require('supertest')
 
-    expect(sum).toBe(6)
+const app = require('../../src/app')
+const { User } = require('../../src/app/models')
+
+describe('Authentication', () => {
+  it('should authenticate with valid credentials', async () => {
+    const user = await User.create({
+      name: 'Gil Santos',
+      email: 'gil@email.com',
+      password_hash: '123456'
+    })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123456'
+      })
+
+    expect(response.status).toBe(200)
   })
 })
