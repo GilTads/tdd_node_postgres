4a504279935cf7d55dd4c39301e0dc463aac3df6
gilTads <gil.tads@gmail.com>
2020-05-14 20:04:12 -0400

    config tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 
 const app = require('../../src/app')
-const { User } = require('../../src/app/models')
+const factory = require('../factories')
 const truncate = require('../utils/truncate')
 
 describe('Authentication', () => {
@@ -9,11 +9,36 @@ describe('Authentication', () => {
     await truncate()
   })
   it('should authenticate with valid credentials', async () => {
-    const user = await User.create({
-      name: 'Gil Santos',
-      email: 'gil@email.com',
-      password: '123456'
+    const user = await factory.create('User', {
+      password: '123132'
     })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123132'
+      })
+
+    expect(response.status).toBe(200)
+  })
+  it('should not authenticate with invalid credentials', async () => {
+    const user = await factory.create('User', {
+      password: '1df15d1f'
+    })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '1sdsd23456'
+      })
+
+    expect(response.status).toBe(401)
+  })
+
+  it('should return jwt token when authenticated', async () => {
+    const user = await factory.create('User')
 
     const response = await request(app)
       .post('/sessions')
@@ -22,6 +47,6 @@ describe('Authentication', () => {
         password: '123456'
       })
 
-    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('token')
   })
 })
